c9d0d93953f7f1b55fc8a6fbe64bb00f0b618efd
amir <amirrezazadeh.job@gmail.com>
2025-10-29 13:33:27 +0330

    update bg video and add readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+![Preview](./images/demo.PNG)
+
+**وبسایت پروتفولیو**
+
+## 🔗 لینک دمو
+
+<a href="https://amirrezazade.github.io/portfolio/" target="_blank">مشاهده وب‌سایت به صورت زنده</a>
--- index.js
+++ index.js
@@ -7,42 +7,30 @@ let typewriter = new Typewriter(app, {
   delay: 75,
 });
 
-typewriter
-  .typeString("دانشجوی رشته کامپیوتر")
-  .pauseFor(2000)
-  .deleteAll()
-  .typeString("توسعه‌ دهنده‌ی فرانت‌ اند")
-  .pauseFor(2000)
-  .deleteAll()
-  .typeString(
-    '<strong class="bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text font-extrabold text-transparent">عاشق دنیای  برنامه نویسی</strong>'
-  )
-  .pauseFor(2500)
-  .start();
+typewriter.typeString("دانشجوی رشته مهندسی کامپیوتر").pauseFor(2000).deleteAll().typeString("توسعه‌ دهنده‌ی فرانت‌ اند").pauseFor(2000).deleteAll().typeString('<strong class="bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text font-extrabold text-transparent">عاشق دنیای  برنامه نویسی</strong>').pauseFor(2500).start();
 
 window.addEventListener("scroll", activateNavLink);
 window.addEventListener("DOMContentLoaded", activateNavLink);
 
 function activateNavLink() {
-    let current = '';
-    
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        
-        if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
-            current = section.getAttribute('id');
-        }
-    });
+  let current = "";
 
-    navLinks.forEach(link => {
-        link.classList.remove('nav-item-active');
-        if (link.classList.contains(current)) {
-            link.classList.add('nav-item-active');
-        }
-    });
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("nav-item-active");
+    if (link.classList.contains(current)) {
+      link.classList.add("nav-item-active");
+    }
+  });
 }
-
 
 // Initialize Lenis for smooth scroll
 const lenis = new Lenis({
